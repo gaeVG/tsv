61835066cf2d02155ed91a9bce611b387e36fd85
Gaev Cenne <2-gaev@users.noreply.cenne.xyz>
2022-09-14 03:04:04 +0000

    update: remove stream files

--- data/resources/[stream]/[other]/map-satellite/fxmanifest.lua
+++
@@ -1,6 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-
-name 'Satellite Map'
-description 'A modification of the game map for a satellite rendering'
-version '1.0'
--- data/resources/[stream]/[other]/radar/fxmanifest.lua
+++
@@ -1,6 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-
-name 'Radar & Reticule'
-description 'A modification of the radar and the player crosshairs, for a rendering without the status bars under the minimap and the removal of the crosshairs'
-version '1.0'
