eb65ef3087307c0fa2adabeae1f4c016e6d68098
Gaev Cenne <gaev@cenne.xyz>
2022-10-02 17:53:56 +0200

    update: await callback nui manager

--- data/resources/tsv/src/client/lib/nui/manager.ts
+++ data/resources/tsv/src/client/lib/nui/manager.ts
@@ -4,17 +4,12 @@ class NUIManager {
 
   private on() {
     RegisterNuiCallbackType('listener');
-    on('__cfx_nui:listener', (data: any, callback: (...args: unknown[]) => void) => {
-      const event = this.manager.find((listener) => {
-        return data.name === listener.name;
-      });
+    on('__cfx_nui:listener', async (data: any, callback: (...args: unknown[]) => void) => {
+      const event = this.manager.find((listener) => data.name === listener.name);
 
       if (event) {
-        const eventHandle = event.handler(data.payload);
-
-        if (callback) {
-          callback({ eventHandle });
-        }
+        const eventHandler = await event.handler(data.payload);
+        callback(eventHandler);
 
         if (event.removeAfterTriggered) {
           this.manager = this.manager.filter((listener) => listener !== event);
