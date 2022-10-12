b061fbe0ee3e9e19378b03c303a4323e5578bcac
Gaev Cenne <gaev@cenne.xyz>
2022-10-12 03:55:35 +0200

    fix: function type

--- data/resources/tsv/src/core/utils/debounce.ts
+++ data/resources/tsv/src/core/utils/debounce.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/ban-types
-function debounce(func: Function, timeout = 300) {
+function debounce(func: () => void, timeout = 300) {
   let timer: NodeJS.Timeout;
   return (...args: unknown[]) => {
     clearTimeout(timer);
